1538316830b3a489ee675d10a5e4e91f91ad937a
Harton <hartontw@gmail.com>
2019-03-19 02:11:40 +0100

    Delayed logger

--- logger.js
+++ logger.js
@@ -5,7 +5,7 @@ const logs = fs.existsSync('logs.json') ? JSON.parse(fs.readFileSync('logs.json'
 
 module.exports = function(install_dir) {
     const logsPath = path.join(install_dir, '/logs.0/');
-    fs.watch(logsPath, (event, filename) => {
+    setTimeout(fs.watch(logsPath, (event, filename) => {
         if (filename) {
             const content = logs[filename] || '';
             const file = fs.readFileSync(path.join(logsPath, filename)).toString();
@@ -19,5 +19,5 @@ module.exports = function(install_dir) {
             logs[filename] = file;
             fs.writeFileSync('logs.json', JSON.stringify(logs));
         } else console.log(event);
-    });
+    }), 5000);
 };
